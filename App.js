6c2d773d0358bc528b4c31d3f7071e12b4ceeb36
Diogo Vilela <diogovlm@gmail.com>
2022-02-14 11:50:00 -0300

    Basic game logic implemented

--- App.js
+++ App.js
@@ -1,34 +1,85 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { colors } from './src/constants';
+import { colors, CLEAR, ENTER } from './src/constants';
 import Keyboard from './src/components/Keyboard';
 
 const NUMBER_OF_TRIES = 6;
+
+//Literally copy an array so we can use it to update a state
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+};
 
 export default function App() {
   const word = 'hello';
   const letters = word.split(''); // ['h', 'e', 'l', 'l', 'o']
 
-  const rows = new Array(NUMBER_OF_TRIES).fill(
-    new Array(letters.length).fill('')
-  ); //generate dinamically rows with blank valuesn based on number of tries and word length
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill(''))
+  ); //generate dinamically rows with blank values based on number of tries and word length
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+
+  //Function to decide what to do according to the key pressed
+  const onKeyPressed = (key) => {
+    const updatedRows = copyArray(rows);
+
+    if (key === CLEAR) {
+      const prevCol = curCol - 1;
+      if (prevCol >= 0) {
+        updatedRows[curRow][prevCol] = '';
+        setRows(updatedRows);
+        setCurCol(prevCol);
+      }
+      return;
+    }
+
+    if (key === ENTER) {
+      if (curCol === rows[0].length) {
+        setCurRow(curRow + 1);
+        setCurCol(0);
+      }
+      return;
+    }
+
+    if (curCol < rows[0].length) {
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
+    }
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  };
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style='light' />
       <Text style={styles.title}>WORDLE</Text>
       <View style={styles.map}>
-        {rows.map((row) => (
-          <View style={styles.row}>
-            {row.map((cell) => (
-              <View style={styles.cell}>
+        {rows.map((row, i) => (
+          <View key={`row-${i}`} style={styles.row}>
+            {row.map((cell, j) => (
+              <View
+                key={`cell-${i}=${j}`}
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.lightgrey
+                      : colors.darkgrey,
+                  },
+                ]}
+              >
                 <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
               </View>
             ))}
           </View>
         ))}
       </View>
-      <Keyboard />
+      <Keyboard onKeyPressed={onKeyPressed} />
     </SafeAreaView>
   );
 }
